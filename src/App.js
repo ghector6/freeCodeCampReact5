f0e6aa21132dd8917e0106df347061cabdb14d43
Hector Garcia Rosales <ghectormus@gmail.com>
2024-01-10 12:46:20 -0600

    add: image for freecodecamp logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 
 function App() {
   return (
-    <div className="App">
-      React
+    <div className="task-app">
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          src={freeCodeCampLogo}
+        className="freecodecamp-logo"
+        alt=""
+        />
+      </div>
     </div>
   );
 }
