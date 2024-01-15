38b0ed09c2be9cc44379bf2fd1bf69b5a8198d2d
Hector Garcia Rosales <ghectormus@gmail.com>
2024-01-15 15:39:46 -0600

    add: structrue for task component

---
+++ src/components/Task.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Task({ text }) {
+  return (
+
+    <div className="task-container">
+      <div className="task-text">
+        {text}
+      </div>
+      <div className="task-icon">
+       Delete
+      </div>
+    </div>
+  )
+}
+
+export default Task;
